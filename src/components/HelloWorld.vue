107b375b0a4b1b6d05dc33bc20f79d1e22540b23
Evgenii Buivolov <zhekadreamteam11@gmail.com>
2021-05-21 04:48:16 +0300

    update: chat

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,10 +4,21 @@
     <!--    <button @click="setState">Задать состояние</button>-->
     <!--    <button @click="getData">Получить состояние</button>-->
     <!--    {{ result }}-->
-    <label>
-      Адрес сервера:
-      <input v-model="baseUrl">
-    </label>
+    <!--    <label>-->
+    <!--      Адрес сервера:-->
+    <!--      <input v-model="baseUrl">-->
+    <!--    </label>-->
+    <div>
+      <label>
+        Автор
+        <input v-model="author">
+      </label>
+      <label>
+        Сообщение
+        <input v-model="message">
+      </label>
+      <button @click="sendMessage">Отправить</button>
+    </div>
     <div v-for="message in chat.messages">
       {{ message }}
     </div>
@@ -21,6 +32,8 @@ import axios from 'axios';
 export default defineComponent({
   data() {
     return {
+      author: '',
+      message: '',
       result: {},
       state: 0,
       count: 0,
@@ -32,6 +45,14 @@ export default defineComponent({
     this.getChat();
   },
   methods: {
+    sendMessage() {
+      axios.post(this.baseUrl + 'message',
+          {
+            author: this.author,
+            message: this.message
+          }
+      ).then(res => console.log(res));
+    },
     getChat() {
       axios.get(this.baseUrl + 'chat').then(res => this.chat = res.data);
     },
